a7cc89c09ecdd93a13851421407508371918ef6e
devasish <devasish@mishra.pro>
2025-10-01 21:23:29 +0530

    Initialize Sidebar

--- app/page.jsx
+++ app/page.jsx
@@ -1,21 +1,22 @@
 'use client';
 import { assets } from "@/assets/assets";
+import Sidebar from "@/components/Sidebar";
 import Image from "next/image";
 import { useState } from "react";
 
 export default function Home() {
 
-  const [expands, setExpand] = useState(false)
+  const [expand, setExpand] = useState(false)
   const [messages, setMessages] = useState([])
   const [isLoading, setIsLoading] = useState(false)
 
   return (
     <div>
       <div className="flex h-screen">
-        {/* -- sidebar -- */}
+        <Sidebar expand={expand} setExpand={setExpand}/>
         <div className="flex-1 flex flex-col items-center justify-center px-4 pb-8 bg-[#292a2d] text-white relative">
           <div className="md:hidden absolute px-4 top-6 flex items-center justify-between w-full">
-            <Image onClick={() => (expands ? setExpand(false) : setExpand(true))}
+            <Image onClick={() => (expand ? setExpand(false) : setExpand(true))}
             className="rotate-180" src={assets.menu_icon} alt=""/>
             <Image className="opacity-70" src={assets.chat_icon} alt=""/>
           </div>
